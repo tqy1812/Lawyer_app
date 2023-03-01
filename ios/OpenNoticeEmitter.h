7a4e50d627f198168443bcc5af85e2798ff5c7f6
yu kuai <yukuai@192.168.30.206>
2023-03-01 15:17:38 +0800

    ios

---
+++ ios/OpenNoticeEmitter.h
@@ -0,0 +1,12 @@
+//
+//  OpenNoticeEmitter.h
+//  LawyerApp
+//
+//  Created by yu kuai on 2023/2/27.
+//
+
+#ifndef OpenNoticeEmitter_h
+#define OpenNoticeEmitter_h
+
+
+#endif /* OpenNoticeEmitter_h */
---
+++ ios/OpenNoticeEmitter.m
@@ -0,0 +1,8 @@
+//
+//  OpenNoticeEmitter.m
+//  LawyerApp
+//
+//  Created by yu kuai on 2023/2/27.
+//
+
+#import <Foundation/Foundation.h>
