f93ebea1ffd964208134077d520feb11687f1cab
tqy <tuqianying123@063.com>
2023-03-15 10:44:19 +0800

    自动登录

--- src/page/LoginPage.js
+++ src/page/LoginPage.js
@@ -126,10 +126,10 @@ class LoginPage extends Component {
                 this.setState({ phone: savedUser.phone, password: savedUser.password });
             }
         }
-        // let autoLogin = await Storage.getAutoLogin();
+        let autoLogin = await Storage.getAutoLogin();
         // logger("autoLogin" + autoLogin)
         // if (autoLogin === '1') {
-        // this.setState({autoLogin: true});
+        this.setState({autoLogin: true});
         if (savedUser.phone && savedUser.password) {
             const that = this;
             requestAnimationFrame(() => that.handleLogin());
