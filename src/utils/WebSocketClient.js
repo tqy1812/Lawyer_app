1204d53ff72912795c8e3dab08c44311f7ce5b66
tqy <tuqianying123@063.com>
2023-04-10 15:55:23 +0800

    生产/开发

--- src/utils/WebSocketClient.js
+++ src/utils/WebSocketClient.js
@@ -4,9 +4,9 @@ import BackgroundTimer from 'react-native-background-timer';
 import NetInfo from '@react-native-community/netinfo';
 import { logger } from './utils';
 import * as Storage from '../common/Storage';
-// const url = 'ws://192.168.30.96:15674/ws';
-// const url = 'ws://59.52.103.97:15674/ws';
-const url = 'wss://ws.kykyai.cn/ws';
+import Common from "../common/constants";
+
+const url = Common.socketUrl;
 const name = 'ky_root';
 const password = 'KYlawyer2023';
 const host = 'lawyer_vhost';
@@ -43,7 +43,7 @@ export default class WebSocketClient {
             // if(!WebSocketClient.subscription) {
           DeviceEventEmitter.emit('wsBind', WebSocketClient.userId);
             // }
- 
+
         },
         onDisconnect: function (frame) {
           logger('STOMP onDisconnect details: ' + frame.body);
@@ -98,18 +98,18 @@ export default class WebSocketClient {
       if(state.isConnected) {
         if(!WebSocketClient.ws) {
           logger('############', WebSocketClient.ws)
-          WebSocketClient.ws = new Client(this.stompConfig);  
+          WebSocketClient.ws = new Client(this.stompConfig);
           WebSocketClient.ws.activate();
         }
         // if (!WebSocketClient.ws || !WebSocketClient.ws.webSocket) {
-        //   WebSocketClient.ws = new Client(this.stompConfig);  
+        //   WebSocketClient.ws = new Client(this.stompConfig);
         // }
         // logger('############'+WebSocketClient.ws.connected + "" + WebSocketClient.ws.active + '.....id' + id)
         // if(!WebSocketClient.ws.connected) {
         //   WebSocketClient.ws.activate();
         // } else if (WebSocketClient.ws.webSocket && WebSocketClient.ws.webSocket.readyState!==1){
         //   logger('############'+WebSocketClient.ws.webSocket.readyState)
-        //   WebSocketClient.ws = new Client(this.stompConfig);  
+        //   WebSocketClient.ws = new Client(this.stompConfig);
         //   WebSocketClient.ws.activate();
         // }
       }
@@ -160,9 +160,9 @@ export default class WebSocketClient {
         if (this.isBackground && WebSocketClient.ws && WebSocketClient.ws.webSocket && WebSocketClient.ws.webSocket.readyState === 1) {
           WebSocketClient.ws.webSocket.send('\x0A');
           logger('>>> PING');
-        } 
+        }
         // else {
-        //   WebSocketClient.ws = new Client(this.stompConfig);  
+        //   WebSocketClient.ws = new Client(this.stompConfig);
         //   WebSocketClient.ws.activate();
         // }
       }
