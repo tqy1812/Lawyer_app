eb86efe46049024ae1c1f5bf012c8d5a46599d71
tqy <tuqianying123@063.com>
2023-04-20 10:35:20 +0800

    计划修改改回

--- src/components/MyPlanItem.js
+++ src/components/MyPlanItem.js
@@ -61,13 +61,13 @@ export default class MyPlanItem extends BaseComponent {
     const {recoding, item} = this.state;
     //logger(this.props.caseList, this.props.caseList[item.case.id+''][2])
     return (
-      <View style={styles.listItemView} activeOpacity={1} >
-        <TouchableOpacity onLongPress={() => this.setFinishTime(item)}  onPressOut={()=>this.setFinishTimeEnd(item)} style={styles.listItemTimeView}><Text style={styles.listItemTimeStart}>{item.start_time ? moment(item.start_time).format('HH:mm') : '-- : --'}</Text><Text style={styles.listItemTimeEnd}>{item.end_time ? moment(item.end_time).format('HH:mm') : '-- : --'}</Text></TouchableOpacity>
+      <TouchableOpacity style={styles.listItemView} activeOpacity={1}  onLongPress={() => this.setFinishTime(item)}  onPressOut={()=>this.setFinishTimeEnd(item)}>
+        <View style={styles.listItemTimeView}><Text style={styles.listItemTimeStart}>{item.start_time ? moment(item.start_time).format('HH:mm') : '-- : --'}</Text><Text style={styles.listItemTimeEnd}>{item.end_time ? moment(item.end_time).format('HH:mm') : '-- : --'}</Text></View>
         <View style={[styles.listItemTimeSplit, {backgroundColor: this.props.caseList[item.case.id+''] ? this.props.caseList[item.case.id+''][2] : '#ff0000',}]}></View>
         <View style={styles.listItemRightView}><Text numberOfLines={1} ellipsizeMode={'tail'} style={styles.listItemTitle}>{item.name}</Text><Text numberOfLines={1} ellipsizeMode={'tail'} style={styles.listItemContent}>{item.case.name}</Text></View>
         <View style={styles.listItemNoticeView}><MyButton style={styles.setNoticeView} onPress={() => {this.changeEnable(item)}}><IcomoonIcon name='alert_0' size={30} color={item.is_wakeup ? '#007afe' : '#fff'} /></MyButton></View>
         { recoding && <View style={styles.waveView}><Wave height={35} width={6} lineColor={'#fff'}></Wave></View> }
-      </View>
+      </TouchableOpacity>
     );
   }
 }
