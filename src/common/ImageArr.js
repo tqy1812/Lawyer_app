e628bb5235c5d438d092ddd63fe278f9b525be68
tqy <tuqianying123@063.com>
2023-05-31 10:33:26 +0800

    图片

--- src/common/ImageArr.js
+++ src/common/ImageArr.js
@@ -1,7 +1,13 @@
 export default {
     // loginBg:require('../images/login_bg.jpg'),
-    logo:require('../images/logo.png'),
     voice:require('../images/voice.png'),
     pic:require('../images/pic.png'),
     wifi:require('../images/wifi.png'),
+    arrow_right:require('../images/center_right.png'),
+    dropdown_down:require('../images/dropdown_down.png'),
+    dropdown_up:require('../images/dropdown_up.png'),
+    custom_menu_center:require('../images/custom_menu_center.png'),
+    custom_menu_report:require('../images/custom_menu_report.png'),
+    input:require('../images/input.png'),
+    microphone:require('../images/microphone.png'),
 }
